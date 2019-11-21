45c9e31b9e24f29fca55804dcbdc09211a4c30d5
Swati meena <44138111+swati210994@users.noreply.github.com>
2019-11-21 10:25:24 +0530

    Update cannyedges.m

--- cannyedges.m
+++ cannyedges.m
@@ -2,11 +2,7 @@ clc;
 clear all;
 close all;
 
-i=imread('E:\ip codes\project\test1.jpg');
-
+i=imread('test1.jpg');
 img=rgb2gray(i);
-lpf = ones(5,5)/25;
-% filtered_img = imfilter(img, lpf);
-
 edge_detected = edge(img,'canny');
 imshow(edge_detected);
